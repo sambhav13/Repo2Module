f2786c00a09584c2f6d957c054a70d97e41a24a8
sambhav.gupta <sambhav.gupta@thoughtworks.com>
2021-01-31 02:32:03 +0530

    Initial commit

---
+++ Test.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+import "github.com/sambhav13/Repo1/mypkg1"
+
+func main() {
+	fmt.Println("vim-go")
+	fmt.Println(mypkg1.GetPkg1String())
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module Repo2Module
+
+go 1.13
+
+require github.com/sambhav13/Repo1 v1.0.0
